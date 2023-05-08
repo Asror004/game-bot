cac4b38ddb8b5945c353645accac243598ed33fc
developer <asroryuldashev57@gmail.com>
2023-05-09 00:22:40 +0500

    Project added

---
+++ src/main/java/dev/asror/botgame/BotGameApplication.java
@@ -0,0 +1,13 @@
+package dev.asror.botgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/asror/botgame/BotGameApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.asror.botgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
